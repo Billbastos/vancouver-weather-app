e5f4116256204680cb745103add0493228b537cd
Guilherme Batista Bastos <gui.b.bastos@gmail.com>
2016-09-10 17:12:41 -0700

    Gruntfile configuration and css structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,21 +9,35 @@ module.exports = function(grunt) {
   // Clean Folder ================================
   config.clean = [distPath, publicJsPath, publicCssPath, publicImgPath];
 
-  // Concat ================================
-  config.concat = {
-    utils: {
-      src: [
-        "src/js/model/FiveDaysThreeHoursDao.js",
-        "src/js/dao/FiveDaysThreeHoursModel.js",
-        "src/js/services/FiveDaysThreeHoursService.js",
-      ],
-      dest: distPath + "app.js"
-    },
-    css: {
-      src: "src/css/*.css",
-      dest: distPath + "app.css"
-    }
+  // Watch ================================
+  config.watch = {
+     browserify: {
+       files: ["src/js/**/*.js"],
+       tasks: ["browserify"]
+     }
+   };
 
+   // Concat ================================
+   config.concat = {
+     css: {
+       src: "src/css/**/*.css",
+       dest: distPath + "app.css"
+     }
+   };
+
+  // Browserify ================================
+  config.browserify = {
+     dist: {
+       options: {
+          transform: [['babelify', {presets: ['es2015']}]]
+       },
+       src: [
+         "src/js/model/FiveDaysThreeHoursDao.js",
+         "src/js/dao/FiveDaysThreeHoursModel.js",
+         "src/js/services/FiveDaysThreeHoursService.js",
+       ],
+       dest: distPath + "app.js",
+     }
   };
 
   // Minification ================================
@@ -55,18 +69,6 @@ module.exports = function(grunt) {
       files: [{
         //'public/images/download.svg' : 'src/images/download.svg'
       }]
-    }
-  };
-
-  // JSHint =====================================
-  config.jshint = {};
-  config.jshint.src = {
-    options: {
-      laxcomma: true,
-      ignores: ["src/**/jquery*.js"]
-    },
-    files: {
-      dist: ["src/**/*.js"]
     }
   };
 
@@ -104,15 +106,16 @@ module.exports = function(grunt) {
   grunt.initConfig(config);
 
   grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-browserify");
   grunt.loadNpmTasks("grunt-contrib-uglify");
-  grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-contrib-connect");
   grunt.loadNpmTasks("grunt-contrib-jasmine");
   grunt.loadNpmTasks("grunt-css");
   grunt.loadNpmTasks("grunt-svgmin");
 
-  grunt.registerTask("default", ["clean", "jshint", "concat", "uglify", "cssmin", "svgmin"]);
+  grunt.registerTask("default", ["clean", "concat", "browserify", "uglify", "cssmin", "svgmin"]);
   grunt.registerTask("tests", ["default", "connect:server", "jasmine"]);
   grunt.registerTask("testsJasmine", ["connect:server", "jasmine"]);
 };
