43ef2b5a0184d6e6fecab71495f50ca88715f708
Guilherme Batista Bastos <gui.b.bastos@gmail.com>
2016-09-13 10:14:33 -0700

    DAO renamed

--- src/js/dao/FiveDaysThreeHoursDao.js
+++ src/js/dao/FiveDaysThreeHoursDao.js
@@ -1,7 +1,34 @@
 class FiveDaysThreeHoursDao {
 
     constructor() {
-        
+        this._url = "http://api.openweathermap.org/data/2.5/forecast?q=Vancouver,us&appid=411b84e404537fea26739061f4a36729";
+        Object.freeze(this);
+    }
+
+    list() {
+
+        return new Promise((resolve, reject)=>{
+
+            let xhttp = new XMLHttpRequest();
+
+            xhttp.onreadystatechange = function() {
+                try {
+                    if (this.readyState == 4 && this.status == 200) {
+                        let data = JSON.parse(this.responseText);
+                        resolve(data.list);
+                    } else if (this.readyState == 4 && this.status != 200) {
+                        reject(Error(this.statusText));
+                    }
+                } catch(e){
+                    reject(Error(e));
+                }
+            };
+
+            xhttp.open("GET", this._url, true);
+            xhttp.send();
+
+        });
+
     }
 
 }
